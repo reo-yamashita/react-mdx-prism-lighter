051630ccc26a785ccdd980821ca07f24666824e7
なーこぉ <nearcloser0215@gmail.com>
2021-05-31 11:28:29 +0900

    Delete index.js

--- src/helper/index.js
+++
@@ -1,8 +0,0 @@
-export const range = (start, end) => {
-    let list = [];
-    for (let i = start; i <= end; i++) {
-        list.push(i);
-    }
-    return list;
-};
-//# sourceMappingURL=index.js.map
